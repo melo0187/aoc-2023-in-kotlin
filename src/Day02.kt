e8f4dc7d6162d479e7a688f056e5573dadf28392
Carmelo Scollo <carmelo.scollo@gmail.com>
2023-12-03 15:06:35 +0100

    Solve Day02 - Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -87,17 +87,32 @@ fun main() {
         return recordsOfPossibleGames.sumOf(GameRecord::id)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    /**
+     * The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
+     * For each game, find the minimum set of cubes that must have been present.
+     * What is the sum of the power of these sets?
+     */
+    fun part2(input: List<GameRecord>): Int {
+        val powerOfSets:List<Int> = input.map { record ->
+            val minimumRequiredReds: Int = record.revealedCubeSubsets.maxOf(GameRecord.RevealedCubeSubset::revealedRed)
+            val minimumRequiredGreens: Int = record.revealedCubeSubsets.maxOf(GameRecord.RevealedCubeSubset::revealedGreens)
+            val minimumRequiredBlues: Int = record.revealedCubeSubsets.maxOf(GameRecord.RevealedCubeSubset::revealedBlues)
+
+            minimumRequiredReds * minimumRequiredGreens * minimumRequiredBlues
+        }
+
+        return powerOfSets.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    val parsedDay02GameRecords = testInput.map(GameRecord::fromInputLine)
-    check(parsedDay02GameRecords == expectedDay02testGameRecords).also { "Passed parsing Day02_test.txt test".println() }
-    check(part1(parsedDay02GameRecords) == 8).also { "Passed calculating result for Day02 test".println() }
+    val day2testGameRecords = testInput.map(GameRecord::fromInputLine)
+    check(day2testGameRecords == expectedDay02testGameRecords).also { "Passed parsing Day02_test.txt test".println() }
+    check(part1(day2testGameRecords) == 8).also { "Passed calculating part1 result for Day02 test".println() }
+    check(part2(day2testGameRecords) == 2286).also { "Passed calculating part2 result for Day02 test".println() }
 
     val input = readInput("Day02")
-    part1(input.map(GameRecord::fromInputLine)).println()
-    part2(input).println()
+    val day2gameRecords = input.map(GameRecord::fromInputLine)
+    part1(day2gameRecords).println()
+    part2(day2gameRecords).println()
 }
